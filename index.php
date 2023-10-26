392a7d499d4bfe172801f9a48a2c08b159515e36
Vignesh Varma <s3835928@csitprdap03.int.its.rmit.edu.au>
2023-10-26 16:13:41 +1100

    REST Web-service function and made sitemap.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php require_once("functions.php") ?>
         <title>Assignment 3</title>
         <link rel="stylesheet" href="a3.css">
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
@@ -12,19 +13,17 @@
 </html>
 
 <body>
-    <div class="footer">
-        <div>
-            <h2>Assignment 3</h2>
-            <ul class="sitemap">
-                <li><a href="index.php">View all Staff</a></li>
-                <li><a href="create.php">Create Course</a></li>
-            </ul>
-        </div>
-        <div>
-            <h2>Github</h2>
-            <ul class="sitemap">
-                <li><a href="https://github.com/rmit-wp-s2-2023/s3835928-a3">Repository</a></li>
-            </ul>
-        </div>
-    </div>
+
+    <?php
+    $data = json_decode($response, true);
+    foreach ($data as $items) {
+        echo "Staff ID: " . $items['staffID'] . "<br>";
+        echo "First Name: " . $items['firstName'] . "<br>";
+        echo "Last Name: " . $items['lastName'] . "<br>";
+        echo "Email: " . $items['email'] . "<br><br>";
+    }
+    ?>
+
+
+    <?php require_once("sitemap.php") ?>
 </body>
---
+++ sitemap.php
@@ -0,0 +1,15 @@
+<div class="footer">
+    <div>
+        <h2>Assignment 3</h2>
+        <ul class="sitemap">
+            <li><a href="index.php">View all Staff</a></li>
+            <li><a href="create.php">Create Course</a></li>
+        </ul>
+    </div>
+    <div>
+        <h2>Github</h2>
+        <ul class="sitemap">
+            <li><a href="https://github.com/rmit-wp-s2-2023/s3835928-a3">Repository</a></li>
+        </ul>
+    </div>
+</div>
