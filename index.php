c7d54cb0ada78d00464216e1a984fb587f56bd25
Vignesh Varma <s3835928@csitprdap03.int.its.rmit.edu.au>
2023-10-26 15:26:38 +1100

    completed sitemap with little CSS

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Assignment 3</title>
+        <link rel="stylesheet" href="a3.css">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+    </head>
+
+</html>
+
+<body>
+    <div class="footer">
+        <div>
+            <h2>Assignment 3</h2>
+            <ul class="sitemap">
+                <li><a href="index.php">View all Staff</a></li>
+                <li><a href="create.php">Create Course</a></li>
+            </ul>
+        </div>
+        <div>
+            <h2>Github</h2>
+            <ul class="sitemap">
+                <li><a href="https://github.com/rmit-wp-s2-2023/s3835928-a3">Repository</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
